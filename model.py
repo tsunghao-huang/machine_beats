e1f808d6b9883bfecaa938fbf3e0ab75a6004c21
Tsunghao Huang <tsunghao_huang@Tsunghaos-MacBook-Pro.local>
2019-03-03 17:16:22 +0100

    1st train

--- model.py
+++ model.py
@@ -2,8 +2,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding
 from keras.layers import LSTM
 from keras.utils import np_utils
-from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
-
 def build_stateless_lstm(input_window_len, n_feature, output_shape, dropout=0.3, neurons_per_layer=None):
     model = Sequential()
 
@@ -15,6 +13,8 @@ def build_stateless_lstm(input_window_len, n_feature, output_shape, dropout=0.3,
         for i, neurons in enumerate(neurons_per_layer):
             if i == 0:
                 model.add(LSTM(neurons_per_layer[0], input_shape=(input_window_len, n_feature), return_sequences=True, dropout=dropout))
+            elif i == len(neurons_per_layer) - 1:
+                model.add(LSTM(neurons, dropout=dropout))
             else:
                 model.add(LSTM(neurons, return_sequences=True, dropout=dropout))
     model.add(Dense(output_shape, activation='softmax'))

bff15d7edb4531a24061067eed0b620aaea6b3f1
Tsunghao Huang <tsunghao_huang@Tsunghaos-MacBook-Pro.local>
2019-05-12 12:55:32 +0200

    format codes, requirements.txt

--- model.py
+++ model.py
@@ -1,17 +1,22 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding, LSTM, Conv1D, MaxPooling1D, Flatten
 from keras.utils import np_utils
+
+
 def build_stateless_lstm(input_window_len, n_feature, output_shape, dropout=0.5, neurons_per_layer=None):
     model = Sequential()
 
     if neurons_per_layer == None:
-        model.add(LSTM(input_window_len, input_shape=(input_window_len, n_feature), dropout=dropout))
+        model.add(LSTM(input_window_len, input_shape=(
+            input_window_len, n_feature), dropout=dropout))
     elif len(neurons_per_layer) == 1:
-        model.add(LSTM(neurons_per_layer[0], input_shape=(input_window_len, n_feature), dropout=dropout))
+        model.add(LSTM(neurons_per_layer[0], input_shape=(
+            input_window_len, n_feature), dropout=dropout))
     elif len(neurons_per_layer) > 1:
         for i, neurons in enumerate(neurons_per_layer):
             if i == 0:
-                model.add(LSTM(neurons_per_layer[0], input_shape=(input_window_len, n_feature), return_sequences=True, dropout=dropout))
+                model.add(LSTM(neurons_per_layer[0], input_shape=(
+                    input_window_len, n_feature), return_sequences=True, dropout=dropout))
             elif i == len(neurons_per_layer) - 1:
                 model.add(LSTM(neurons, dropout=dropout))
             else:
@@ -21,16 +26,18 @@ def build_stateless_lstm(input_window_len, n_feature, output_shape, dropout=0.5,
 
     return model
 
+
 def build_CNN(input_window_len, n_feature, output_shape):
-  
-  model = Sequential()
-  model.add(Conv1D(filters=32, kernel_size=32, activation='relu', input_shape=(input_window_len, n_feature)))
-  model.add(MaxPooling1D(pool_size=2))
+
+    model = Sequential()
+    model.add(Conv1D(filters=32, kernel_size=32, activation='relu',
+                     input_shape=(input_window_len, n_feature)))
+    model.add(MaxPooling1D(pool_size=2))
 #   model.add(Conv1D(filters=32, kernel_size=16, activation='relu'))
 #   model.add(MaxPooling1D(pool_size=2))
-  model.add(Flatten())
-  model.add(Dense(16, activation='relu'))
-  model.add(Dense(output_shape, activation='sigmoid'))
-  model.compile(loss='binary_crossentropy', optimizer='adam')
-  
-  return model
+    model.add(Flatten())
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(output_shape, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam')
+
+    return model
--- train.py
+++ train.py
@@ -7,21 +7,24 @@ from keras.utils import np_utils
 input_window_len = 32
 pred_steps = 1
 
-df, tracks_len_list = concat_all_midi_to_df(root_dir = './LOG_4_4', return_tracks_len_list=True)
+df, tracks_len_list = concat_all_midi_to_df(
+    root_dir='./LOG_4_4', return_tracks_len_list=True)
 
 X_train, y_train, X_test, y_test, instruments_list = prepare_data(df, input_window_len=input_window_len, pred_steps=1, overlaps=0, train_test_split=0.1,
                                                                   tracks_len_list=tracks_len_list, max_instruments=6)
 
 n_feature = X_train.shape[2]
 output_shape = y_train.shape[1]
-model = build_stateless_lstm(input_window_len=input_window_len, n_feature=n_feature, output_shape=output_shape, dropout=0.5, neurons_per_layer=[32,32,32])
+model = build_stateless_lstm(input_window_len=input_window_len, n_feature=n_feature,
+                             output_shape=output_shape, dropout=0.5, neurons_per_layer=[32, 32, 32])
 
 # Experiment with different batch_size
 # batch_size = 256
 es = EarlyStopping(monitor='val_loss', patience=20)
-mc = ModelCheckpoint(filepath='./new_encode_1st_try.h5', monitor='val_loss', verbose=1,save_best_only=True)
+mc = ModelCheckpoint(filepath='./new_encode_1st_try.h5',
+                     monitor='val_loss', verbose=1, save_best_only=True)
 #tb = TensorBoard(log_dir="./logs/lstm_{}".format(time()))
 callbacks = [es, mc]
 
-history = model.fit(X_train, y_train, epochs=50, callbacks=callbacks, 
+history = model.fit(X_train, y_train, epochs=50, callbacks=callbacks,
                     validation_split=0.1, verbose=1, shuffle=False)
